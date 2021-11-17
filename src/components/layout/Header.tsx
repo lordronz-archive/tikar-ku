04e781da0c36da5c3fa4e40b3f48ba27f1fb63ff
LordRonz <aarontanhar2000@gmail.com>
2021-11-17 23:55:41 +0700

    feat: better header, some cleanup

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,13 +9,13 @@ import tikarku from '../../../public/tikarku.png';
 const Header = () => {
   return (
     <header className='transition-shadow sticky top-0 z-50'>
-      <nav className='flex items-center justify-center border'>
+      <nav className='flex items-center justify-center border-b-4 border-vblack border-opacity-30 space-x-4'>
         <div className='flex p-4'>
           <Image src={tikarku} alt='Logo TikarKU' height={tikarku.height / 10} width={tikarku.width / 10} />
         </div>
         <Input />
-        <FaBell size='1.5em' />
-        <FiShoppingCart size='1.5em' />
+        <FaBell size='1.5em' className='cursor-pointer' />
+        <FiShoppingCart size='1.5em' className='cursor-pointer' />
       </nav>
     </header>
   );
