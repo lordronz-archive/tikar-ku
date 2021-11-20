1771589d89686d05d367ae3f676ac3fe9bb4f2b6
LordRonz <aarontanhar2000@gmail.com>
2021-11-20 23:44:00 +0700

    feat: Add search result

---
+++ src/components/form/Filter.tsx
@@ -0,0 +1,91 @@
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+
+const Filter = () => {
+  const [harga, setHarga] = useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setHarga(event.target.value);
+  };
+
+  return (
+    <div className='p-6 border rounded-xl border-vblack border-b-2 border-r-2 border-l-2 border-t-0 shadow-lg border-opacity-20'>
+      <div className='flex items-center justify-between'>
+        <Typography variant='h5'>
+          <Box fontWeight='fontWeightBold' color='#989898'>
+            Filter
+          </Box>
+        </Typography>
+        <Typography>
+          <Box fontWeight='fontWeightBold' className='text-vgreen cursor-pointer hover:underline'>
+            reset
+          </Box>
+        </Typography>
+      </div>
+      <div>
+        <FormControl variant="standard" sx={{ my: 1, minWidth: 180 }}>
+          <InputLabel id="demo-simple-select-standard-label">Urutkan Harga</InputLabel>
+          <Select
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard"
+            value={harga}
+            onChange={handleChange}
+            label="Urutkan Harga"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div>
+        <FormControl variant="standard" sx={{ my: 1, minWidth: 180 }}>
+          <InputLabel id="demo-simple-select-standard-label">Lokasi</InputLabel>
+          <Select
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard"
+            value={harga}
+            onChange={handleChange}
+            label="Lokasi"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div>
+        <FormControl variant="standard" sx={{ my: 1, minWidth: 180 }}>
+          <InputLabel id="demo-simple-select-standard-label">Rating</InputLabel>
+          <Select
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard"
+            value={harga}
+            onChange={handleChange}
+            label="Rating"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { FaBell } from 'react-icons/fa';
 import { FiShoppingCart } from 'react-icons/fi';
 
@@ -11,7 +12,9 @@ const Header = () => {
     <header className='transition-shadow sticky top-0 z-50 bg-vwhite'>
       <nav className='flex items-center justify-center border-b-4 border-vblack border-opacity-30 space-x-4'>
         <div className='flex p-4'>
-          <Image src={tikarku} alt='Logo TikarKU' height={tikarku.height / 10} width={tikarku.width / 10} />
+          <Link href='/'>
+            <a><Image src={tikarku} alt='Logo TikarKU' height={tikarku.height / 10} width={tikarku.width / 10} /></a>
+          </Link>
         </div>
         <Input />
         <FaBell size='1.5em' className='cursor-pointer' />
