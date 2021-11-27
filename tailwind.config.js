aa95ad6351091159819c3fb41b18812d1d6f590d
LordRonz <aarontanhar2000@gmail.com>
2021-11-27 16:26:10 +0700

    feat: Refined payment page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,20 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    colors: {
-      'vgreen': '#23bb86',
-      'vblack': '#0b132a',
-      'vwhite': '#fff',
-      'vyellow': '#ffce31',
+    extend: {
+      colors: {
+        'vgreen': '#23bb86',
+        'vblack': '#0b132a',
+        'vblue': '#2A74B3',
+        'vwhite': '#fff',
+        'vyellow': '#ffce31',
+      },
+      maxWidth: {
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+      },
     },
-    extend: {},
   },
   variants: {
     extend: {},
